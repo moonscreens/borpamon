dc9b16e65741452fa0c14b4c43206040994de487
Caleb Babin <object.caleb@gmail.com>
2021-08-20 17:04:57 -0500

    download more gracefully

--- get-borpamon.js
+++ get-borpamon.js
@@ -13,19 +13,23 @@ const downloadFile = (async (url, path) => {
     const out = fs.createWriteStream(path)
     const stream = canvas.createPNGStream()
     stream.pipe(out)
+    console.log(`Downloaded ${path}`);
 });
 
 
 const borpadex = {};
+let count = 0;
 fetch('https://sheets.googleapis.com/v4/spreadsheets/1rEePpILD6k5x8oY9_QIutsxYS8qcmn2E9u2fDhS9HgI/values/!A1:E905?key=' + process.env.GOOGLE_API_KEY)
     .then(res => res.json())
     .then(json => {
         for (let index = 1; index < json.values.length; index++) {
             const element = json.values[index];
             if (element[3]) {
-                console.log(element);
+                count++;
                 const imagePath = `/borpas/${element[0]}.png`;
-                downloadFile(element[3], `${__dirname}${imagePath}`);
+                setTimeout(() => {
+                    downloadFile(element[3], `${__dirname}${imagePath}`);
+                }, 50 * index);
                 borpadex[element[0]] = {
                     number: element[0],
                     originalName: element[1],
@@ -37,6 +41,7 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/1rEePpILD6k5x8oY9_QIutsxYS8
         }
 
         fs.writeFileSync(`${__dirname}/borpas/borpadex.json`, JSON.stringify(borpadex, null, 4));
+        console.log(`Saved ${count} borpas`);
     })
 
 
