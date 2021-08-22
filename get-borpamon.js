f8c32809a86495f9c6679b5a069778fda595e862
Caleb Babin <object.caleb@gmail.com>
2021-08-22 10:07:09 -0500

    update scripts

--- get-borpamon.js
+++ get-borpamon.js
@@ -2,6 +2,40 @@ const { createCanvas, loadImage } = require('canvas');
 require('dotenv').config();
 const fetch = require('node-fetch');
 const fs = require('fs');
+const { google } = require('googleapis');
+
+const drive = google.drive({
+    version: 'v3',
+    auth: process.env.GOOGLE_API_KEY,
+});
+
+const getFromDrive = (fileId, dest) => {
+    return new Promise((resolve, reject) => {
+        drive.files.get({
+            fileId,
+            alt: 'media',
+            mimeType: 'image/png',
+            key: process.env.GOOGLE_API_KEY,
+        }, {
+            responseType: 'stream',
+        }, (err, res) => {
+            if (err) {
+                console.log('initial error')
+                reject(err);
+            } else {
+                res.data
+                    .on('end', () => {
+                        resolve();
+                    })
+                    .on('error', err => {
+                        console.log('promise error')
+                        reject(err);
+                    })
+                    .pipe(fs.createWriteStream(dest))
+            }
+        });
+    });
+};
 
 const downloadFile = (async (url, path) => {
     if (url.toLowerCase().includes('drive.google.com')) {
@@ -9,10 +43,15 @@ const downloadFile = (async (url, path) => {
         for (let index = 0; index < parts.length; index++) {
             if (parts[index] === "d") {
                 try {
-                    let googleResponse = await fetch(`https://www.googleapis.com/drive/v2/files/${parts[index+1]}?key=${process.env.GOOGLE_API_KEY}`);
+                    //await getFromDrive(parts[index + 1], path);
+                    //url = 'data:image/png;base64,' + fs.readFileSync(path).toString('base64');
+                    url = `https://www.googleapis.com/drive/v2/files/${parts[index + 1]}?key=${process.env.GOOGLE_API_KEY}&alt=media&source=downloadUrl`;
+                    /*let googleResponse = await fetch(`https://www.googleapis.com/drive/v2/files/${parts[index+1]}?key=${process.env.GOOGLE_API_KEY}`);
                     googleResponse = await googleResponse.json();
-                    url = googleResponse.downloadUrl;
+                    console.log(googleResponse);
+                    url = googleResponse.downloadUrl;*/
                 } catch (e) {
+                    console.error("Google Drive error");
                     console.error(e);
                     console.log('the URL ', url, 'failed to fetch info from google drive');
                     return;
@@ -50,18 +89,18 @@ const downloadFile = (async (url, path) => {
 
 const borpadex = {};
 let count = 0;
-fetch('https://sheets.googleapis.com/v4/spreadsheets/1rEePpILD6k5x8oY9_QIutsxYS8qcmn2E9u2fDhS9HgI/values/!A:E?key=' + process.env.GOOGLE_API_KEY)
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1rEePpILD6k5x8oY9_QIutsxYS8qcmn2E9u2fDhS9HgI/values/!A:F?key=' + process.env.GOOGLE_API_KEY)
     .then(res => res.json())
     .then(json => {
         for (let index = 2; index < json.values.length; index++) {
             const element = json.values[index];
-            if (element.length >= 4 && element[3] && element[3].toLowerCase().includes('http')) {
+            if (element.length >= 4 && element[4] && element[4].toLowerCase().includes('http')) {
                 count++;
                 const imagePath = `/borpas/${element[0]}.png`;
-                if (!fs.existsSync(`${__dirname}${imagePath}`) || true) {
+                if (!fs.existsSync(`${__dirname}${imagePath}`)) {
                     setTimeout(() => {
-                        downloadFile(element[3], `${__dirname}${imagePath}`);
-                    }, 500 * count);
+                        downloadFile((element[4] && element[4] !=="") ? element[4] : element[3], `${__dirname}${imagePath}`);
+                    }, 125 * count);
                 }
                 if (borpadex[element[0]] !== undefined) {
                     console.log(`${element[0]} already exists`);
@@ -71,7 +110,7 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/1rEePpILD6k5x8oY9_QIutsxYS8
                     originalName: element[1],
                     name: element[2],
                     imagePath: imagePath,
-                    artist: element[4],
+                    artist: element[5],
                 }
             }
         }
